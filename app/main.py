54886d13d68d2c6f1b8572b3bf9906c6eeb59a95
Anastasiia Duzhynska <a.duzhynska@gmail.com>
2026-01-08 15:44:08 +0200

    Fix flake8

--- app/main.py
+++ app/main.py
@@ -7,20 +7,24 @@ BASE_URL = "http://api.weatherapi.com/v1/current.json"
 API_KEY = os.getenv("API_KEY")
 CITY = "Paris"
 
+
 def get_weather() -> None:
     if not API_KEY:
         print("Error: Please set API_KEY environment variable")
         sys.exit(1)
 
     try:
-        response = requests.get(BASE_URL, params={"key": API_KEY, "q": CITY}, timeout=10)
+        response = requests.get(
+            BASE_URL,
+            params={"key": API_KEY, "q": CITY},
+            timeout=10
+        )
         response.raise_for_status()
         weather = response.json()
 
         print(f"Weather in {weather['location']['name']} right now:")
         print(f"Temperature: {weather['current']['temp_c']}°C")
         print(f"Condition: {weather['current']['condition']['text']}")
-
 
     except requests.exceptions.RequestException as e:
         print(f"Network error: {e}")
